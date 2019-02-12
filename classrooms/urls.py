44e571b4894e05d8d1a7cdb17dec1aec0e89f822
Mohammed Algharaballi <malgharaballi@Mohammeds-MacBook-Pro.local>
2019-02-12 15:01:31 +0300

    Submission

--- classrooms/urls.py
+++ classrooms/urls.py
@@ -6,13 +6,19 @@ from django.conf.urls.static import static
 from classes import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('classrooms/', views.classroom_list, name='classroom-list'),
-    path('classrooms/<int:classroom_id>/', views.classroom_detail, name='classroom-detail'),
+	path('admin/', admin.site.urls),
+	path('classrooms/', views.classroom_list, name='classroom-list'),
+	path('classrooms/<int:classroom_id>/', views.classroom_detail, name='classroom-detail'),
 
-    path('classrooms/create', views.classroom_create, name='classroom-create'),
-    path('classrooms/<int:classroom_id>/update/', views.classroom_update, name='classroom-update'),
-    path('classrooms/<int:classroom_id>/delete/', views.classroom_delete, name='classroom-delete'),
+	path('classrooms/create', views.classroom_create, name='classroom-create'),
+	path('classrooms/<int:classroom_id>/update/', views.classroom_update, name='classroom-update'),
+	path('classrooms/<int:classroom_id>/delete/', views.classroom_delete, name='classroom-delete'),
+	path('classrooms/<int:classroom_id>/student/create', views.student_create, name='student-create'),
+	path('classrooms/<int:classroom_id>/classroom/create', views.classroom_create, name='classroom-create'),
+	path('signin/', views.signin, name='signin'),
+	path('signout/', views.signout, name='signout'),
+	path('signup/', views.signup, name='signup'),
+
 ]
 
 if settings.DEBUG:
